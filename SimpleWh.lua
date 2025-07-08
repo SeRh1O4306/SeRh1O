a0f62ef94895e2dbe7db02f306c0ad21384f98d8
Sergey <brawlstarshack9999@gmail.com>
2025-07-08 04:46:59 +0300

    Update SimpleWh.lua

--- SimpleWh.lua
+++ SimpleWh.lua
@@ -1,2 +1,2 @@
 --Simple roblox wallhack(may have some troubles)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SeRh1O4306/SeRh1O/refs/heads/main/SimpleWh.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeRh1O4306/SeRh1O/refs/heads/main/settingsforadmins.lua"))()
